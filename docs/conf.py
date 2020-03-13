f90e33914267169dda18cf21a89dee5e052f17c5
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-13 06:05:56 +0000

    Add custom CSS for code cells (#36)

--- docs/conf.py
+++ docs/conf.py
@@ -64,6 +64,7 @@ html_logo = "_static/logo.jpg"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 
 intersphinx_mapping = {"python": ("https://docs.python.org/3.7", None)}
