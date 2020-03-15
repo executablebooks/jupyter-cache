95017812af123f0f6dc3127d0f12e9517485978b
Chris Sewell <chrisj_sewell@hotmail.com>
2020-03-15 07:31:18 +0000

    resize and compress logo

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = [
 # a list of builtin themes.
 #
 html_theme = "pandas_sphinx_theme"
-html_logo = "_static/logo.jpg"
+html_logo = "_static/logo_small.jpg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
